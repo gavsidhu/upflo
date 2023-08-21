099f1351e2be77e47ea399e480053954c55e8bc1
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-08-21 01:45:57 -0700

    removed comment

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,37 +4,16 @@ import { Ticket } from "../entity/HelpDesk/Ticket.entity";
 import { Contact } from "../entity/Contacts/Contact.entity";
 import { List } from "../entity/Contacts/List.entity";
 
-/**
- * Class representing a `DatabaseConnection`.
- *
- * @class
- */
 export class DatabaseConnection {
-  /**
-   * The source of the data.
-   *
-   * @private
-   * @type {DataSource}
-   */
   private dataSource: DataSource;
 
-  /**
-   * Creates a new `DatabaseConnection` class with options.
-   *
-   * @param {DataSourceOptions} options - The `DataSource` options.
-   */
   constructor(options: DataSourceOptions) {
     this.dataSource = new DataSource({
       ...options,
-      entities: [Tag, Ticket, Contact, List], // The list of entities to be linked with the data source.
+      entities: [Tag, Ticket, Contact, List],
     });
   }
 
-  /**
-   * Initializes the data source asynchronously.
-   *
-   * @returns {Promise<void>} - Promise represents the completion of an asynchronous operation.
-   */
   async connect(): Promise<void> {
     try {
       await this.dataSource.initialize();
@@ -44,11 +23,6 @@ export class DatabaseConnection {
     }
   }
 
-  /**
-   * Gets the existing initialized data source connection.
-   *
-   * @returns {DataSource} - The existing initialized data source connection.
-   */
   getConnection(): DataSource {
     return this.dataSource;
   }
