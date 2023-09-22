ff2b451b285d401f41e7b96b6ef615cace2a32f5
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-09-22 15:02:37 -0700

    renamed email types file

--- types/Email.ts
+++
@@ -1,7 +0,0 @@
-import { MailgunClientOptions } from "mailgun.js";
-
-export interface EmailProvider {
-  sendEmail(to: string, subject: string, body: string): Promise<any>;
-}
-
-export interface MailgunProviderConfig extends MailgunClientOptions {}
