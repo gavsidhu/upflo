9017f3d7be97b85218cabe0ad423a2c90a19708f
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-09-22 15:32:01 -0700

    added new entitites to datasource

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,6 +3,11 @@ import { Tag } from "../entity/Contacts/Tag.entity";
 import { Ticket } from "../entity/HelpDesk/Ticket.entity";
 import { Contact } from "../entity/Contacts/Contact.entity";
 import { List } from "../entity/Contacts/List.entity";
+import { EmailEvents } from "../entity/Workflow/EmailEvents.entity";
+import { Workflow } from "../entity/Workflow/Workflow.entity";
+import { WorkFlowQueue } from "../entity/Workflow/WorkflowQueue.entity";
+import { Note } from "../entity/Contacts/Note.entity";
+import { WorkflowQueueArchive } from "../entity/Workflow/WorkflowQueueArchive.entity";
 
 export class DatabaseConnection {
   private dataSource: DataSource;
@@ -10,7 +15,17 @@ export class DatabaseConnection {
   constructor(options: DataSourceOptions) {
     this.dataSource = new DataSource({
       ...options,
-      entities: [Tag, Ticket, Contact, List],
+      entities: [
+        Tag,
+        Ticket,
+        Contact,
+        List,
+        Note,
+        EmailEvents,
+        Workflow,
+        WorkFlowQueue,
+        WorkflowQueueArchive,
+      ],
     });
   }
 
