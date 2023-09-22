9d532c661c3a6af8da5d3e97e21c68eee8ffdd6e
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-09-22 15:27:57 -0700

    added workflows service to Upflo class

--- src/services/Upflo.ts
+++ src/services/Upflo.ts
@@ -4,12 +4,14 @@ import { Contacts } from "./Contacts/Contacts";
 import { HelpDesk } from "./HelpDesk";
 import { Email } from "./Email/Email";
 import { EmailProvider } from "../../types/Email";
+import { Workflow } from "./Workflow/Workflow";
 
 export class Upflo {
   private dbConnection: DatabaseConnection;
   public helpdesk: HelpDesk;
   public contacts: Contacts;
   public email: Email;
+  public workflows: Workflow;
 
   constructor(options: DataSourceOptions) {
     this.dbConnection = new DatabaseConnection(options);
@@ -19,5 +21,9 @@ export class Upflo {
     this.contacts = new Contacts(this.dbConnection.getConnection());
     this.helpdesk = new HelpDesk(this.dbConnection.getConnection());
     this.email = new Email(this.dbConnection.getConnection(), emailProvider);
+    this.workflows = new Workflow(
+      this.dbConnection.getConnection(),
+      this.email
+    );
   }
 }
